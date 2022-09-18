91288f619e01bb20596fa3bb05cdbadc034c2b68
Parsa361 <parsafarahany821@gmail.com>
2022-09-19 00:25:03 +0430

    Nav full responsive, Add hamburger menu (js code)

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -4,10 +4,10 @@
         <!-- First nav -->
         <div class="bg-white text-textGray">
             <div
-                class="container mx-auto h-[4.625rem] pt-[0.75rem] flex flex-row-reverse sm:flex-row justify-between items-center px-1">
+                class="container mx-auto h-[4.625rem] pt-[0.75rem] flex flex-row-reverse md:flex-row justify-between items-center px-1">
 
                 <!-- Logo -->
-                <div class="hidden sm:flex sm:mx-0">
+                <div class="hidden md:flex sm:mx-0">
                     <Logo />
                 </div>
 
@@ -34,43 +34,61 @@
 
 
                 <!-- Login Button -->
-                <div class="mx-auto sm:mx-0">
+                <div>
                     <button class="hover:border-b-4 hover:text-black border-textGray text-md md:text-[1.5rem]">
                         <p>ورود | ثبت نام</p>
                     </button>
                 </div>
+
+                <!-- Menu button -->
+                <div class="flex md:hidden items-center hover:shadow-lg shadow-black">
+                    <button @click="showMobileMenu = !showMobileMenu" class="py-4 px-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-7 h-7 text-textGray">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75" />
+                        </svg>
+                    </button>
+                </div>
+
             </div>
         </div>
 
 
         <!-- Second nav -->
         <div class="bg-bgBg text-textGray">
-            <div class="container mx-auto md:h-[2.3125rem] md:border w-full border-yellowHood rounded-b-xl "
-                id="navbar-default">
+            <div class="md:block container mx-auto md:h-[2.3125rem] md:border border-yellowHood rounded-b-xl "
+                id="navbar-default" :class="{hidden: showMobileMenu}">
                 <ul
-                    class="flex flex-col justify-center space-y-1 text-center text-sm items-center pt-[0.25rem] pb-[0.25rem]   md:flex-row md:text-[1rem] md:gap-20 md:justify-center md:items-center">
+                    class="flex flex-col justify-center space-y-1 text-center text-sm items-center w-full pt-[0.25rem] pb-[0.25rem] md:flex-row md:text-[1rem] md:gap-20 md:justify-center md:items-center">
 
-                    <li>
+                    <li
+                        class="w-screen hover:text-white hover:bg-yellowHood sm:hover:none md:w-fit md:rounded-full md:px-2">
                         <NuxtLink to="/" class="hidden md:block">دسته بندی ها</NuxtLink>
                     </li>
 
-                    <li>
-                        <NuxtLink to="/" class="block">مقالات</NuxtLink>
+                    <li
+                        class="w-screen hover:text-white hover:bg-yellowHood border-white sm:hover:none md:w-fit md:rounded-full md:px-2">
+                        <NuxtLink to="/" class="block w-full">مقالات</NuxtLink>
                     </li>
 
-                    <li>
-                        <NuxtLink to="/about" class="block">مزیت های ما</NuxtLink>
+                    <li
+                        class="w-screen hover:text-white hover:bg-yellowHood border-white sm:hover:none md:w-fit md:rounded-full md:px-2">
+                        <NuxtLink to="/service" class="block">خدمات ما</NuxtLink>
                     </li>
 
-                    <li>
-                        <NuxtLink to="/" class="block">ارتباط با ما</NuxtLink>
+                    <li
+                        class="w-screen hover:text-white hover:bg-yellowHood border-white sm:hover:none md:w-fit md:hover:rounded-full md:px-2">
+                        <NuxtLink to="/contact" class="block">ارتباط با ما</NuxtLink>
                     </li>
 
-                    <li>
-                        <NuxtLink to="/" class="block">درباره ما</NuxtLink>
+                    <li
+                        class="w-screen hover:text-white hover:bg-yellowHood border-white sm:hover:none md:w-fit md:hover:rounded-full md:px-2">
+                        <NuxtLink to="/about" class="block">درباره ما</NuxtLink>
                     </li>
 
-                    <li>
+                    <li
+                        class="w-screen hover:text-white hover:bg-yellowHood border-white sm:hover:none md:w-fit md:hover:rounded-full md:px-2">
                         <NuxtLink to="/" class="block md:hidden lg:block">پست های اینستاگرام</NuxtLink>
                     </li>
 
@@ -91,3 +109,8 @@ export default {
 
 </script>
 
+<script setup>
+import { ref } from "vue";
+
+const showMobileMenu = ref(true)
+</script>
