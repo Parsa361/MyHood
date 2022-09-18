3ac33e16f767a82f0629c103f1b39c2631534d7b
Parsa361 <parsafarahany821@gmail.com>
2022-09-18 14:50:23 +0430

    Add some meta tags for SEO, add HomeSlider tag

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,24 @@
 <template>
-  <div>
-    <p></p>
-  </div>
+  <HomeSlider />
 </template>
 
 <script>
+import HomeSlider from '~/components/HomeSlider.vue';
 
 export default {
   name: "IndexPage",
-  components: {},
+  components: { HomeSlider },
 
+  head: {
+    title: 'Home page',
+    meta: [
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Home page description'
+      }
+    ],
+  }
 }
-
 
 </script>
