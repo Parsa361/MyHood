b96970cb11c812c3a450d2726a5bd428840e2494
Parsa361 <parsafarahany821@gmail.com>
2022-09-23 23:14:30 +0330

    reinstall flowbite and fix it

---
+++ plugins/utils.client.js
@@ -0,0 +1,5 @@
+import Vue from 'vue'
+import flowbite from 'flowbite'
+
+
+Vue.use(flowbite)
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-      "./components/**/*.{js,vue,ts}",
-      "./layouts/**/*.vue",
-      "./pages/**/*.vue",
-      "./plugins/**/*.{js,ts}",
-      "./nuxt.config.{js,ts}",
-      "./node_modules/flowbite/**/*.js",
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+
   ],
   theme: {
     extend: {
@@ -23,5 +23,7 @@ module.exports = {
       VazirMedium: ["Vazir-Medium"],
     }
   },
-  plugins: [require('flowbite/plugin')],
+  plugins: [
+    require('flowbite/plugin'),
+  ],
 }
