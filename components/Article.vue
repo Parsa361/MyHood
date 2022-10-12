260bfb64834589e7628556a96fe66a27f063dc77
Parsa361 <parsafarahany821@gmail.com>
2022-10-12 23:29:02 +0330

    Article template component

---
+++ components/Article.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>Here is our articles</h1>
+        <article v-for="article in articles">
+            <img :src="require(`~/assets/images/${article.image}.png`)" :alt="article.image">
+            <h3>
+                {{article.title}}
+            </h3>
+            <p>
+                {{article.content}}
+            </p>
+
+        </article>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ArticleCom',
+    props: [
+        'articles',
+    ]
+}
+</script>
