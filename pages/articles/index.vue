aa18cf4c99887c4c94b028c3ad0daa3bd16f3038
Parsa361 <parsafarahany821@gmail.com>
2022-10-12 23:29:38 +0330

    Articles page with just 3 example objects

---
+++ pages/articles/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container mx-auto">
+        <ArticleCom :articles="articles" />
+    </div>
+</template>
+
+<script>
+import ArticleCom from '~/components/Article.vue';
+export default {
+    name: "Articles",
+    data() {
+        return {
+            articles: [
+                {
+                    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero id blanditiis, porro eius sequi iure corrupti.Vel quis id expedita nesciunt dicta, deleniti cupiditate cum, fuga repellendus rerum, similique totam!",
+                    image: "fix-hood",
+                    title: "آموزش مقدماتی تعمیر ایرادات جزعی هود های مخفی"
+                },
+                {
+                    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero id blanditiis, porro eius sequi iure corrupti.Vel quis id expedita nesciunt dicta, deleniti cupiditate cum, fuga repellendus rerum, similique totam!",
+                    image: "clean-hood",
+                    title: "روش تمیز کردن انواع هودها"
+                },
+                {
+                    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero id blanditiis, porro eius sequi iure corrupti.Vel quis id expedita nesciunt dicta, deleniti cupiditate cum, fuga repellendus rerum, similique totam!",
+                    image: "hood-diversity",
+                    title: "هر آنچه درباره هود مناسب آشپزخانه خود باید بدانید"
+                },
+            ]
+        };
+    },
+    components: { ArticleCom }
+}
+</script>
