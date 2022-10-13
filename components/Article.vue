cf7a44ce5cdaac11670130528d42823c38939bca
Parsa361 <parsafarahany821@gmail.com>
2022-10-13 23:53:55 +0330

    Edit article component, how it shows articles

--- components/Article.vue
+++ components/Article.vue
@@ -1,16 +1,17 @@
 <template>
     <div>
-        <h1>Here is our articles</h1>
-        <article v-for="article in articles">
-            <img :src="require(`~/assets/images/${article.image}.png`)" :alt="article.image">
-            <h3>
-                {{article.title}}
-            </h3>
-            <p>
-                {{article.content}}
-            </p>
+        <article v-for="article in articles" class="flex flex-row mb-5 h-[10em] bg-white rounded-tl-xl rounded-bl-xl">
+                <img :src="require(`~/assets/images/${article.image}.png`)" :alt="article.image"
+                    class="w-[10em] h-[10em]">
+            <div class="flex flex-col">
+                <h3 class="mr-5 font-Lalezar font-light mt-2">{{article.title}}</h3>
+                <p class="mr-5 pl-11 font-VazirLight my-auto">{{article.content}}</p>
+            </div>
 
         </article>
+
+
+
     </div>
 </template>
 
