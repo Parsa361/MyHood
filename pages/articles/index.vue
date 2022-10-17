f292819d0ec65b9342a7429ed3eaf60b07ef3a96
Parsa361 <parsafarahany821@gmail.com>
2022-10-17 22:25:26 +0330

    Remove axios, Replace data in script

--- pages/articles/index.vue
+++ pages/articles/index.vue
@@ -19,27 +19,33 @@
 
 <script>
 import ArticleCom from '~/components/Article.vue';
-import axios from 'axios'
 export default {
     name: "Articles",
     data() {
         return {
-            articles: []
+            articles: [
+                {
+                    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero id blanditiis, porro eius sequi iure corrupti.Vel quis id expedita nesciunt dicta, deleniti cupiditate cum, fuga repellendus rerum, similique totam!",
+                    img: "clean-hood",
+                    description: 'هود های قوی',
+                    title: "آموزش مقدماتی تعمیر ایرادات جزعی هود های مخفی"
+                },
+                {
+                    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero id blanditiis, porro eius sequi iure corrupti.Vel quis id expedita nesciunt dicta, deleniti cupiditate cum, fuga repellendus rerum, similique totam!",
+                    img: "fix-hood",
+                    description: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.',
+                    title: "تمام راه های تمیز کردن هود های جزیره ای و هود های مورب"
+                },
+                {
+                    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero id blanditiis, porro eius sequi iure corrupti.Vel quis id expedita nesciunt dicta, deleniti cupiditate cum, fuga repellendus rerum, similique totam!",
+                    img: "diversity-hood",
+                    description: 'هود های سریع و خشن 10',
+                    title: "این هود ها کیفیتش انصافا عالیه بخرید پشیمون نمیشید"
+                },
+            ]
         };
     },
     components: { ArticleCom },
-    mounted() {
-            this.getArticles()
-        },
-        methods: {
-            async getArticles() {
-                try {
-                    const res = await this.$axios.$get('/api/v1/articles');
-                    this.articles= res
-                } catch (err) {
-                    console.log(err)
-                }
-            }
-        }
+
 }
 </script>
