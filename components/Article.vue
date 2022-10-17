2678827bc65afbf5ee9087ad6494124b26f92d4a
Parsa361 <parsafarahany821@gmail.com>
2022-10-17 22:24:51 +0330

    Responsive article template

--- components/Article.vue
+++ components/Article.vue
@@ -1,11 +1,15 @@
 <template>
     <div>
-        <article v-for="article in articles" class="flex flex-row mb-5 h-[10em] bg-white rounded-tl-xl rounded-bl-xl">
-            <img :src="require(`~/assets/images/${article.img}.png`)" :alt="article.img" class="w-[10em] h-[10em]">
+        <article v-for="article in articles"
+            class="flex flex-col md:flex-row mb-5 min-h-[10em] md:bg-white rounded-tl-xl rounded-bl-xl">
+            <img :src="require(`~/assets/images/${article.img}.png`)" :alt="article.img" class="w-[10em] h-[10em] mx-auto md:mx-0">
 
-            <div class="flex flex-col">
-                <h3 class="mr-5 font-Lalezar font-light mt-2">{{article.title}}</h3>
-                <p class="max-w-lg lg:max-w-3xl mr-5 pl-11 font-VazirLight my-auto truncate">{{article.description}}</p>
+            <div class="flex flex-col py-5">
+                <h3 class="text-sm lg:text-xl mr-5 font-Lalezar font-light text-center md:text-start">{{article.title}}</h3>
+
+                <p
+                    class="md:max-w-sm lg:max-w-3xl mr-5 pl-11 font-VazirLight mt-4 text-xs lg:text-lg text-center md:text-start truncate">
+                    {{article.description}}</p>
             </div>
 
 
