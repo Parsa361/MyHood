c81f2a63f7887af847f6cd98c1c2a25c3ab7f56c
Parsa361 <parsafarahany821@gmail.com>
2022-10-18 22:15:31 +0330

    Make demo article's _id page

--- pages/articles/_id/index.vue
+++ pages/articles/_id/index.vue
@@ -1,12 +1,46 @@
 <template>
     <div>
-        {{$route.params.id}}
+        <article v-for="article in articles" :key="article.id" class="container mx-auto">
+            <div v-if="article.id == $route.params.id">
+                <img :src="require(`~/assets/images/${article.img}.png`)" :alt="article.img" class=" mt-5 mx-auto">
+                <h1 class="text-center mt-5 font-Lalezar text-xl">{{article.title}}</h1>
+
+                <p class="mt-[2.5rem] font-VazirBold text-lg">{{article.description}}</p>
+                <hr class="border border-black my-5">
+                <p class="font-VazirLight text-lg">{{article.content}}</p>
+            </div>
+        </article>
     </div>
 </template>
 
 <script>
 export default {
-    //const articleID = this.$route.params.id
-    //.get(`/api/v1/articles/${articleID}/`)
+    data() {
+        return {
+            articles: [
+                {
+                    id: 0,
+                    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero id blanditiis, porro eius sequi iure corrupti.Vel quis id expedita nesciunt dicta, deleniti cupiditate cum, fuga repellendus rerum, similique totam!",
+                    img: "clean-hood",
+                    description: 'هود های قوی',
+                    title: "آموزش مقدماتی تعمیر ایرادات جزعی هود های مخفی"
+                },
+                {
+                    id: 1,
+                    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero id blanditiis, porro eius sequi iure corrupti.Vel quis id expedita nesciunt dicta, deleniti cupiditate cum, fuga repellendus rerum, similique totam!",
+                    img: "fix-hood",
+                    description: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.',
+                    title: "تمام راه های تمیز کردن هود های جزیره ای و هود های مورب"
+                },
+                {
+                    id: 2,
+                    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero id blanditiis, porro eius sequi iure corrupti.Vel quis id expedita nesciunt dicta, deleniti cupiditate cum, fuga repellendus rerum, similique totam!",
+                    img: "diversity-hood",
+                    description: 'هود های سریع و خشن 10',
+                    title: "این هود ها کیفیتش انصافا عالیه بخرید پشیمون نمیشید"
+                },
+            ]
+        }
+    }
 }
 </script>
