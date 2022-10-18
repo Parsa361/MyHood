e2d3a05d301e052403b0efa34b143995980335fd
Parsa361 <parsafarahany821@gmail.com>
2022-10-18 22:10:21 +0330

    Add border radious to the text field

--- components/CategoriesSec.vue
+++ components/CategoriesSec.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!-- Categories text with 2 yellow Svg -->
-    <div class=" my-[1rem] mt-[4rem] mx-auto flex flex-row items-center justify-center font-Lalezar gap-5">
+    <div class="md:my-[1rem] md:mt-[4rem] mx-auto flex flex-row items-center justify-center font-Lalezar gap-5">
       <svg xmlns="http://www.w3.org/2000/svg" width="111" height="3" class="flex w-[5rem] md:w-[7rem]"
         viewBox="0 0 111 3">
         <line id="Line_2" data-name="Line 2" x2="108" transform="translate(1.5 1.5)" fill="none" stroke="#fcb900"
@@ -15,48 +15,46 @@
       </svg>
     </div>
 
-    <!-- Background white -->
-    <div class="bg-white">
-      <!-- Container -->
-      <div class="container mx-auto my-[3.5rem] flex flex-col mb-[0.5rem]" v-for="(cat, index) in category" :key="index"
-        :class="index % 2 === 1 ? 'md:flex-row-reverse' : 'md:flex-row' ">
 
-        <!-- Image 1-->
-        <div class="md:w-1/2">
-          <img :src="require(`~/assets/images/${cat.catImg}.png`)" alt="hidden hood"
-            class="object-cover object-center h-full" />
-        </div>
-        <!-- Text 1-->
-        <div class="flex flex-col md:w-1/2 pr-2 pl-4 py-2">
-          <p class="text-center md:text-start font-Lalezar text-[0.75rem] xl:mb-[1rem] md:text-[1.5rem]">
-            {{cat.catTitle}}</p>
-          <p
-            class="hidden lg:flex text-center md:text-start font-VazirLight text-[0.75rem] md:text-[1rem] xl:mb-[1rem]">
-            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
-            استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله
-            در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد
-            نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد،
-            کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان
-            جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای
-            طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان
-            فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری
-            موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد
-            نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل
-            دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
+    <!-- Container -->
+    <div class="container mx-auto my-[3.5rem] flex flex-col mb-[0.5rem]" v-for="(cat, index) in category" :key="index"
+      :class="index % 2 === 1 ? 'md:flex-row-reverse' : 'md:flex-row'">
+
+      <!-- Image 1-->
+      <div class="md:w-1/2">
+        <img :src="require(`~/assets/images/${cat.catImg}.png`)" alt="hidden hood"
+          class="object-cover object-center h-full" />
+      </div>
+      <!-- Text 1-->
+      <div class="flex flex-col md:w-1/2 py-2 bg-white rounded-lg"
+        :class="index % 2 === 0 ? 'md:mr-5 md:pr-2': 'md:ml-5 md:pr-2'">
+        <p class="text-center md:text-start font-Lalezar text-[0.75rem] xl:mb-[1rem] md:text-[1.5rem]">
+          {{cat.catTitle}}</p>
+        <p class="hidden lg:flex text-center md:text-start font-VazirLight text-[0.75rem] md:text-[1rem] xl:mb-[1rem]">
+          لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
+          استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله
+          در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد
+          نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد،
+          کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان
+          جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای
+          طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان
+          فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری
+          موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد
+          نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل
+          دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
+        </p>
+        <p class="lg:hidden text-center md:text-start font-VazirLight text-[0.75rem] md:text-[1rem]">
+          لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
+          استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله
+          در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد
+          نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد،
+          کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان
+          جامعه و متخصصان را می طلبد</p>
+        <button
+          class="mx-auto md:ml-[3rem] mt-[0.5rem] md:mt-[0.25rem] mr-auto w-[7rem] lg:w-[10rem] h-[2rem] lg:h-[3rem] bg-yellowHood rounded-tr-lg rounded-bl-lg hover:shadow-lg hover:drop-shadow-lg hover:shadow-yellow-400">
+          <p class="text-[0.75rem] lg:text-[1rem] text-center text-textGray mb-1 px-1 hover:text-white">جزئیات بیشتر
           </p>
-          <p class="lg:hidden text-center md:text-start font-VazirLight text-[0.75rem] md:text-[1rem]">
-            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
-            استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله
-            در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد
-            نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد،
-            کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان
-            جامعه و متخصصان را می طلبد</p>
-          <button
-            class="mx-auto md:ml-[3rem] mt-[0.5rem] md:mt-[0.25rem] mr-auto w-[7rem] lg:w-[10rem] h-[2rem] lg:h-[3rem] bg-yellowHood rounded-tr-lg rounded-bl-lg hover:shadow-lg hover:drop-shadow-lg hover:shadow-yellow-400">
-            <p class="text-[0.75rem] lg:text-[1rem] text-center text-textGray mb-1 px-1 hover:text-white">جزئیات بیشتر
-            </p>
-          </button>
-        </div>
+        </button>
       </div>
     </div>
   </div>
