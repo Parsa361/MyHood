dfc537d25737588ce037f91fdd62d4d63b5cfe64
Parsa361 <parsafarahany821@gmail.com>
2022-11-06 10:13:29 +0330

    Make slider auto slide

--- components/HomeSlider.vue
+++ components/HomeSlider.vue
@@ -3,7 +3,7 @@
     <div>
         <div id="animation-carousel"
             class="relative container mx-auto mt-[0.25rem] sm:mt-[1.5rem] md:mt-0 lg:mt-[1.5rem]"
-            data-carousel="static">
+            data-carousel="slide">
             <!-- Carousel wrapper -->
             <div class="relative h-40 overflow-hidden rounded-lg md:h-[18rem] lg:h-[20rem] xl:h-[24rem]">
 
