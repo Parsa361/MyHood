63b001dc2b6174730a713f24a55811f1aa6e9934
Parsa361 <parsafarahany821@gmail.com>
2022-11-06 11:49:59 +0330

    Add store for token setting

---
+++ store/index.js
@@ -0,0 +1,36 @@
+import axios from 'axios'
+
+
+export const state = () => ({
+    user: {
+        id: 0,
+        username: '',
+        token: '',
+    },
+    isAuthenticated: false,
+})
+
+export const getters = {}
+
+export const mutations = {
+    initializeStore(state) {
+        const token = localStorage.getItem('token')
+        if (token) {
+            state.user.token = token
+            state.isAuthenticated = true
+            axios.defaults.headers.common.Authorization = 'Token ' + token
+        }
+    },
+    setUser(state, user) {
+        state.user.id = user.id
+        state.user.username = user.username
+        state.isAuthenticated = true
+    },
+    removeUser(state) {
+        state.user.id = 0
+        state.user.username = 'user.username'
+        state.isAuthenticated = false
+    }
+}
+
+export const actions = {}
