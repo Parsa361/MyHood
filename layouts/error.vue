5c737ceeb6fbdad65667891d530cf07547a1e122
Parsa Farahani <parsafarahany821@gmail.com>
2022-11-09 11:49:56 +0330

    Add error.vue layout

---
+++ layouts/error.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container mx-auto my-10 p-[10em] bg-black rounded-xl">
+        <hr>
+        <p v-if="error.statusCode === 404" class="text-center font-bold text-white">صفحه یافت نشد!!</p>
+        <p class="text-center font-bold text-white" v-else>An error occurred</p>
+        <hr>
+    </div>
+</template>
+  
+<script>
+export default {
+    props: ['error'],
+    layout: 'error'
+}
+</script>
