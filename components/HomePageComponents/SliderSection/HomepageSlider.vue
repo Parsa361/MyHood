cf87ad3cbbf5c4c2909f05363b693b33747e6280
Parsa Farahani <parsafarahany821@gmail.com>
2023-03-24 18:06:13 +0430

    Create Homepage slider with components

---
+++ components/HomePageComponents/SliderSection/HomepageSlider.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="relative overflow-hidden w-full h-[13rem] md:h-[22rem] lg:h-[30rem] mt-8 mb-12 rounded-md">
+        <slot />
+
+        <!-- Buttons -->
+        <button @click="next" class="absolute top-1/2 right-4"><i class="hidden md:block fas fa-chevron-circle-right fa-2xl"
+                style="color: #fcb900;"></i></button>
+        <button @click="prev" class="absolute top-1/2 left-4"><i class="hidden md:block fas fa-chevron-circle-left fa-2xl"
+                style="color: #fcb900;"></i></button>
+    </div>
+</template>
+
+<script>
+import Slides from '../Slides.vue';
+
+export default {
+    components: { Slides },
+    methods: {
+        next() {
+            this.$emit('next')
+        },
+        prev() {
+            this.$emit('prev')
+
+        }
+    }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,72 @@
 <template>
   <div>
-    <HomeSlider />
+    <HomepageSlider @next="onNext" @prev="onPrev">
+      <Slides :direction="direction" :visibleSlide="visibleSlide" v-for="(slide, index) in slides" :key="slide"
+        :index="index">
+        <img :src="slide" alt='image' class="w-full h-full">
+      </Slides>
+    </HomepageSlider>
+
+
     <IconBar />
     <CategoriesSection />
     <ArticleSec />
+    <AboutSec />
   </div>
 </template>
 
 <script>
-import HomeSlider from "~/components/HomeSlider.vue";
-import IconBar from "~/components/Icons.vue";
-import CategoriesSection from "~/components/CategoriesSec.vue";
-import ArticleSec from "~/components/ArticleSec.vue";
+import IconBar from "~/components/HomePageComponents/Icons.vue";
+import CategoriesSection from "~/components/HomePageComponents/CategoriesSec.vue";
+import ArticleSec from "~/components/HomePageComponents/ArticleSec.vue";
+import AboutSec from "~/components/HomePageComponents/AboutSec.vue";
+import HomepageSlider from "~/components/HomePageComponents/SliderSection/HomepageSlider.vue";
+import Slides from "~/components/HomePageComponents/Slides.vue";
 
 export default {
   name: "IndexPage",
-  components: { HomeSlider, IconBar, CategoriesSection, ArticleSec },
+  data() {
+    return {
+      slides: [
+        require('@/static/images/slide4.png'),
+        require('@/static/images/slide3.png'),
+        require('@/static/images/slide2-2.png'),
+        require('@/static/images/Untitled-2.png')
+      ],
+      visibleSlide: 0,
+      direction: 'left'
+    }
+  },
+  computed: {
+    slidesLen() {
+      return this.slides.length
+    }
+  },
+  methods: {
+    onNext() {
+      if (this.visibleSlide >= this.slidesLen - 1) {
+        this.visibleSlide = 0
+      } else this.visibleSlide++
+      this.direction = 'left'
+
+    },
+    onPrev() {
+      if (this.visibleSlide <= 0) {
+        this.visibleSlide = this.slidesLen - 1
+      } else this.visibleSlide--
+      this.direction = 'right'
+
+    },
+    startTimer() {
+      setInterval(() => {
+        this.onNext()
+      }, 4000)
+    }
+  },
+  mounted() {
+    this.startTimer()
+  },
+  components: { IconBar, CategoriesSection, ArticleSec, AboutSec, HomepageSlider, Slides },
   head() {
     return {
       title: "صفحه اصلی MyHood",
@@ -23,7 +74,7 @@ export default {
         {
           hid: "description",
           name: "description",
-          content: "مشاهده محصولات و دسته بندی ها و ارتباط با ما",
+          content: "صفحه اصلی سایت هود من, محصولات زیبا و مقرون به صرفه",
         },
       ],
     };
