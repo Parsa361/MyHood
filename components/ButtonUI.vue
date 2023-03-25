1e5d8993a7e475eec4a7b96c9839075c6d7a6377
Parsa Farahani <parsafarahany821@gmail.com>
2023-03-26 00:49:30 +0430

    Edit the ButtonUI component, Button padding

--- components/ButtonUI.vue
+++ components/ButtonUI.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <button
-            class="inline-block my-1 px-3 py-2 md:px-5 md:py-3 bg-yellowHood text-slate-600 rounded-lg shadow-md hover:bg-yellow-700 hover:text-white active:scale-[0.8] text-[0.75rem] md:text-[1rem] lg:text-[1.25rem]">
+            class="inline-block my-1 px-3 py-2 md:px-5 md:py-3 bg-yellowHood text-slate-600 rounded-lg shadow-md hover:bg-yellowHood/70 hover:text-white active:scale-[0.8] text-[0.75rem] md:text-[1rem] lg:text-[1.25rem]">
             <slot />
         </button>
     </div>
