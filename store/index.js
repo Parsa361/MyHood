164473d124763dff8a02d97672ce8b6917624d6a
Parsa Farahani <parsafarahany821@gmail.com>
2023-03-27 18:17:39 +0430

    Handle all process of authentication, SPA and SSR

--- store/index.js
+++ store/index.js
@@ -1,39 +1,102 @@
-import axios from 'axios'
-
+import Cookie from 'js-cookie'
 
 export const state = () => ({
-    user: {
-        id: 0,
-        username: '',
-        token: '',
-    },
-    isAuthenticated: false,
+    token: null,
+    error: null,
+    loading: null
 })
 
-export const getters = {}
-
 export const mutations = {
-    initializeStore(state) {
-        if (process.client) {
-            const token = localStorage.getItem('token')
-
-            if (token) {
-                state.user.token = token
-                state.isAuthenticated = true
-                axios.defaults.headers.common.Authorization = 'Token ' + token
-            }
-        }
+    setToken(state, token) {
+        state.token = token
     },
-    setUser(state, user) {
-        state.user.id = user.id
-        state.user.username = user.username
-        state.isAuthenticated = true
+    setError(state, error) {
+        state.error = error
     },
-    removeUser(state) {
-        state.user.id = 0
-        state.user.username = 'user.username'
-        state.isAuthenticated = false
+    setLoading(state, loading) {
+        state.loading = loading
+    },
+    clearToken(state) {
+        state.token = null
     }
 }
 
-export const actions = {}
+export const actions = {
+    async authenticateUser(vuexContext, authData) {
+        let authUrl = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + process.env.webAPIKey
+        if (!authData.isLogin) {
+            authUrl = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + process.env.webAPIKey
+        }
+        vuexContext.commit('setLoading', true)
+        await this.$axios.$post(authUrl, {
+            email: authData.email,
+            password: authData.password,
+            returnSecureToken: true
+        })
+            .then(data => {
+                vuexContext.commit('setToken', data.idToken)
+                vuexContext.commit('setError', null)
+                vuexContext.commit('setLoading', false)
+
+                localStorage.setItem('token', data.idToken)
+                localStorage.setItem('tokenExpiration', new Date().getTime() + Number.parseInt(data.expiresIn) * 1000)
+                Cookie.set('jwt', data.idToken)
+                Cookie.set('expirationDate', new Date().getTime() + Number.parseInt(data.expiresIn) * 1000)
+
+
+                console.log(data);
+                console.log('expire time :' + new Date().getTime() + Number.parseInt(data.expiresIn) * 1000);
+            })
+            .catch(e => {
+                vuexContext.commit('setError', e.message)
+                vuexContext.commit('setLoading', false)
+                console.log(e);
+            })
+    },
+    async initAuth(vuexContext, req) {
+        let token;
+        let expirationTime;
+        if (req) {
+            if (!req.headers.cookie) {
+                return;
+            }
+            const jwtCookie = req.headers.cookie.split(';').find(c => c.trim().startsWith('jwt='))
+            if (!jwtCookie) {
+                console.log('jwtCookie in vuex is null');
+                return;
+            }
+            token = jwtCookie.split('=')[1]
+            expirationTime = req.headers.cookie.split(';').find(c => c.trim().startsWith('expirationDate='))
+                .split('=')[1]
+
+        } else {
+            token = localStorage.getItem('token')
+            expirationTime = localStorage.getItem('tokenExpiration')
+            if (!token || new Date().getTime() > Number.parseInt(expirationTime)) {
+                console.log('No token or invalid token!');
+                localStorage.removeItem('token')
+                localStorage.removeItem('tokenExpiration')
+                Cookie.remove('jwt')
+                Cookie.remove('expirationDate')
+                vuexContext.commit('clearToken')
+                return
+            }
+        }
+        vuexContext.commit('setToken', token)
+
+    }
+
+}
+export const getters = {
+    isAuthenticated(state) {
+        return state.token != null
+    },
+    error(state) {
+        return state.error
+    },
+    loading(state) {
+        return state.loading
+    }
+}
+
+
