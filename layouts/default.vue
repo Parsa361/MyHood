97820972e41b77f9a25c68ebe29dadd1494cf0cf
Parsa Farahani <parsafarahany821@gmail.com>
2023-03-27 18:31:05 +0430

    When the SSR happens,loading animation is executed

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,24 +1,34 @@
 <template>
   <div>
     <NavBar />
-
+    <!-- Loading -->
+    <div v-if="isLoading" class="flex items-center justify-center h-screen">
+      <svg class="animate-spin h-12 w-12 text-gray-700" xmlns="http://www.w3.org/2000/svg" fill="none"
+        viewBox="0 0 24 24">
+        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+        <path class="opacity-75" fill="currentColor"
+          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647zM12 20a8 8 0 01-8-8H0c0 6.627 5.373 12 12 12v-4zm4-7.291A7.962 7.962 0 0120 12h4c0 3.042-1.135 5.824-3 7.938l-3-2.647z">
+        </path>
+      </svg>
+    </div>
+    
     <div>
-      <Nuxt class="container mx-auto"/>
+      <Nuxt class="container mx-auto px-1" />
     </div>
 
     <FooterCom />
   </div>
 </template>
 
-<style>
-
-
-</style>
-
 <script>
 import NavBar from "~/components/NavBar.vue";
 import FooterCom from "~/components/FooterCom.vue";
 export default {
+  data() {
+    return {
+      isLoading: false
+    }
+  },
   components: {
     NavBar,
     FooterCom,
@@ -36,11 +46,17 @@ export default {
       },
       { name: "format-detection", content: "telephone=no" },
     ],
-
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
-  beforeCreate() {
-    this.$store.commit("initializeStore");
-  }
+  created() {
+    this.$axios.onRequest(() => {
+      this.isLoading = true
+    })
+    this.$axios.onResponse(() => {
+      this.isLoading = false
+    })
+    this.$axios.onError(() => {
+      this.isLoading = false
+    })
+  },
 };
 </script>
