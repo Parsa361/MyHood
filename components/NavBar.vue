35a5c39b5f5af1170601b096b5e226ef748eeee3
Parsa Farahani <parsafarahany821@gmail.com>
2023-03-27 18:30:04 +0430

    Rewrite Navbar, use responsiveNav component in it

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -2,9 +2,9 @@
     <div class="font-Lalezar">
 
         <!-- First nav -->
-        <div class="bg-white text-textGray">
+        <div class="border-b border-yellowHood text-textGray">
             <div
-                class="container mx-auto h-[4.625rem] pt-[0.75rem] flex flex-row-reverse md:flex-row justify-between items-center px-1">
+                class="container mx-auto h-[5rem] pt-[0.75rem] flex flex-row-reverse md:flex-row justify-between items-center px-1">
                 <!-- Logo -->
                 <div class="hidden md:flex sm:mx-0">
                     <Logo />
@@ -15,8 +15,8 @@
                         <label for="simple-search" class="sr-only">Search</label>
                         <div class="relative w-full">
                             <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                                <svg aria-hidden="true" class="w-5 h-5 text-textGray dark:text-gray-400"
-                                    fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                <svg aria-hidden="true" class="w-5 h-5 text-textGray dark:text-gray-400" fill="currentColor"
+                                    viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                     <path fill-rule="evenodd"
                                         d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
                                         clip-rule="evenodd"></path>
@@ -30,16 +30,11 @@
                 </div>
 
                 <!-- Login Button -->
-                <NuxtLink to="/login">
-                    <button
-                        class="hover:border-b-4 hover:text-black border-textGray text-md md:text-[1.125rem] lg:text-[1.5rem]">
-                        ورود | ثبت نام
-                    </button>
-                </NuxtLink>
+                <LoginCom />
 
                 <!-- Menu button -->
                 <div class="flex md:hidden items-center hover:shadow-lg shadow-black">
-                    <button @click="showMobileMenu = !showMobileMenu" class="py-4 px-2">
+                    <button @click="toggleMobileMenu" class="py-4 px-2">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                             stroke="currentColor" class="w-7 h-7 text-textGray">
                             <path stroke-linecap="round" stroke-linejoin="round"
@@ -52,61 +47,28 @@
         </div>
 
         <!-- Second nav -->
-        <div class="bg-bgBg text-textGray">
-            <div class="md:block container mx-auto md:h-[2.3125rem] md:border border-yellowHood rounded-b-xl md:pt-[0.25rem]"
-                id="navbar-default" :class="{ hidden: showMobileMenu }">
-                <ul
-                    class="flex flex-col justify-center text-center text-sm items-center w-full pt-[0.25rem] pb-[0.25rem] md:flex-row md:text-[1rem] md:gap-12 lg:gap-20 md:justify-center md:items-center">
-
-                    <li
-                        class="w-screen hover:text-white hover:bg-yellowHood sm:hover:none md:w-fit md:rounded-full md:px-2">
-                        <NuxtLink to="/" class="hidden md:block">دسته بندی ها</NuxtLink>
-                    </li>
-
-                    <li
-                        class="w-screen hover:text-white hover:bg-yellowHood border-white sm:hover:none md:w-fit md:rounded-full md:px-2">
-                        <NuxtLink to="/articles" class="block w-full">مقالات</NuxtLink>
-                    </li>
-
-                    <li
-                        class="w-screen hover:text-white hover:bg-yellowHood border-white sm:hover:none md:w-fit md:rounded-full md:px-2">
-                        <NuxtLink to="/service" class="block">خدمات ما</NuxtLink>
-                    </li>
-
-                    <li
-                        class="w-screen hover:text-white hover:bg-yellowHood border-white sm:hover:none md:w-fit md:hover:rounded-full md:px-2">
-                        <NuxtLink to="/contact" class="block">ارتباط با ما</NuxtLink>
-                    </li>
-
-                    <li
-                        class="w-screen hover:text-white hover:bg-yellowHood border-white sm:hover:none md:w-fit md:hover:rounded-full md:px-2">
-                        <NuxtLink to="/about" class="block">درباره ما</NuxtLink>
-                    </li>
-
-                    <li
-                        class="w-screen hover:text-white hover:bg-yellowHood border-white sm:hover:none md:w-fit md:hover:rounded-full md:px-2">
-                        <NuxtLink to="/" class="block">پست های اینستاگرام</NuxtLink>
-                    </li>
-
-                </ul>
-            </div>
-        </div>
+        <ResponsiveNav :showMobileMenu="showMobileMenu" />
     </div>
-
 </template>
 
 <script>
+import LoginCom from "./LoginCom.vue"
 import Logo from "./Logo.vue"
+import ResponsiveNav from "./ResponsiveNav.vue"
 
 export default {
     name: "NavBar",
-    components: { Logo }
+    components: { Logo, ResponsiveNav, LoginCom },
+    data() {
+        return {
+            showMobileMenu: true,
+        }
+    },
+    methods: {
+        toggleMobileMenu() {
+            this.showMobileMenu = !this.showMobileMenu
+        },
+    }
 }
 
-</script>
-
-<script setup>
-import { ref } from "vue";
-
-const showMobileMenu = ref(true)
 </script>
