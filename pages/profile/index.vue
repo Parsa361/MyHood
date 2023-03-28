8af212fcd9ca5b2d3a9cc82e03b666198cb02868
Parsa Farahani <parsafarahany821@gmail.com>
2023-03-29 03:23:32 +0430

    Just add a simple profile page which has a logout

---
+++ pages/profile/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        This is profile user
+
+        <div class="text-center">
+            <button @click="onLogout" class=" inline-block bg-red-500 text-white p-2 rounded-md">خروج</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    middleware: ['auth'],
+    methods: {
+        onLogout() {
+            this.$store.dispatch('logout')
+            this.$router.push('/')
+        },
+
+    }
+}
+</script>
